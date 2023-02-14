7936ed9935a88ab4ac2c8d9a7736bdc4a83636e1
22PHOP2D-Munkh-Uils <22phop2d-munkh-uils@Bilguuns-iMac.local>
2023-02-14 19:11:28 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { DataProvider } from "./controllers/context/Data.Provider";
+import { Header } from "./controllers/Header";
+import { Home } from "./controllers/Home";
+import { Menu } from "./controllers/Menu";
+import {useEffect}from 'react'
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <DataProvider> */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      {/* </DataProvider> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
