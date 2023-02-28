816ad3b16cd3e3dfd3b72f46eb9588896ade14cb
22PHOP2D-Munkh-Uils <22phop2d-munkh-uils@Bilguuns-iMac.local>
2023-02-28 19:21:26 +0800

    MUNKH01 fixed

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,17 +1,17 @@
 import styles from "./styles/Home.module.css";
 import background from "./assets/background.png";
 import heviinBoov from "./assets/heviinboov.jpeg";
+import banana from "./assets/banana.jpeg";
+import { Carousel } from "@mantine/carousel";
+import { useRef } from "react";
+import Autoplay from "embla-carousel-autoplay";
 
 export const Home = () => {
+  const autoplay = useRef(Autoplay({ delay: 2000 }));
   return (
     <div className={styles.controller}>
       <div className={styles.light}>
         <img className={styles.background} src={background}></img>
-      </div>
-      <div class={styles.geeks}>
-        <img
-          src="https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png"
-        />
       </div>
       <div className={styles.contentContainer}>
         <div className={styles.content2Container}>
@@ -25,6 +25,61 @@ export const Home = () => {
               "ӨГӨӨЖ БООВ" ТА ХЭВИЙН БООВОО ЭНДЭЭС АВНА УУ!
             </div>
           </div>
+        </div>
+      </div>
+      <div className={styles.carouselContainer}>
+        <div className={styles.div}>
+          {/* <Carousel
+            className={styles.secondCarousel}
+            withIndicators
+            height={310}
+            slideSize="13%"
+            slideGap="md"
+            breakpoints={[
+              { maxWidth: "lg", slideSize: "20%" },
+              { maxWidth: "md", slideSize: "50%" },
+              { maxWidth: "sm", slideSize: "100%", slideGap: 0 },
+            ]}
+            loop
+            align="start"
+          >
+            <Carousel.Slide className={styles.slide}>1</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>2</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>3</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>4</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>5</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>6</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>7</Carousel.Slide>
+          </Carousel> */}
+
+          <Carousel
+            withIndicators
+            className={styles.secondCarousel}
+            height={400}
+            slideSize="17.45%"
+            slideGap="md"
+            plugins={[autoplay.current]}
+            onMouseEnter={autoplay.current.stop}
+            onMouseLeave={autoplay.current.reset}
+            breakpoints={[
+              { maxWidth: "md", slideSize: "50%" },
+              { maxWidth: "sm", slideSize: "100%", slideGap: 0 },
+            ]}
+            loop
+            align="start"
+          >
+            <Carousel.Slide className={styles.slide}>1</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>2</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>3</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>4</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>5</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>6</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>7</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>8</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>9</Carousel.Slide>
+            <Carousel.Slide className={styles.slide}>10</Carousel.Slide>
+            {/* ...other slides */}
+          </Carousel>
         </div>
       </div>
     </div>
