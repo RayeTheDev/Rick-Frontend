d48a96a4f64c55ca1c6d696ad5f5ed30d1b7bfcb
2022HOP2D-Turbold <turbold@LEAP-8s-iMac.local>
2023-03-04 18:51:22 +0800

    TUR01 fixed

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -1,19 +1,32 @@
 import styles from "../styles/Signup.module.css";
 import { Link } from "react-router-dom";
-import { Button } from '@mantine/core';
-import { TextInput } from '@mantine/core';
-
+import {
+  Button,
+  Group,
+  TextInput,
+  PasswordInput,
+  Radio,
+  FileInput,
+} from "@mantine/core";
+import { IconAt } from "@tabler/icons-react";
 export const Signup = () => {
   return (
     <div className={styles.one}>
       <div className={styles.go_login}>
-        <p className={styles.sign_al}>Already Sign up ?</p>
-        <p className={styles.sign_info}>to keep connected with us please login with your personal info</p>
-        <Link to="/Login">
-          <Button className={styles.btn_1} radius="xl">
-            Settings
-          </Button>
-        </Link>
+        <div className={styles.innerContainer}>
+          <p className={styles.sign_al}>Already Signed up?</p>
+          <div style={{width: "58%", display: "flex",  justifyContent: "center"}}>
+            <p className={styles.sign_info}>
+            To keep connected with us please login with your personal info
+          </p>
+          
+          </div>
+          <Link to="/Login">
+            <Button className={styles.btn_1} radius="xl">
+              Login
+            </Button>
+          </Link>
+        </div>
       </div>
       <div className={styles.just}>
         <div className={styles.signup_page}>
@@ -31,9 +44,39 @@ export const Signup = () => {
             />
           </div>
 
-          <input placeholder="password" className={styles.input}></input>
-          <input placeholder="gender" className={styles.input}></input>
-          <Button className={styles.btn_2} variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }}>Sign Up</Button>
+          <TextInput
+            className={styles.longInput}
+            label="Email"
+            placeholder="Your email"
+            icon={<IconAt size="0.8rem" />}
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            toggleTabIndex={0}
+            className={styles.longInput}
+          />
+          <FileInput
+            className={styles.longInput}
+            label="Upload avatar"
+            placeholder="Upload files"
+            multiple
+          />
+          <Radio.Group name="favoriteFramework" label="Gender:" withAsterisk>
+            <Group mt="sm" className={styles.group}>
+              <Radio value="react" label="Male" />
+              <Radio value="svelte" label="Female" />
+              <Radio value="ng" label="Other" />
+            </Group>
+          </Radio.Group>
+
+          <Button
+            className={styles.btn_2}
+            variant="gradient"
+            gradient={{ from: "teal", to: "blue", deg: 60 }}
+          >
+            Sign Up
+          </Button>
           <div className={styles.goto_href}>
             <a href="l">go to login</a>
           </div>
