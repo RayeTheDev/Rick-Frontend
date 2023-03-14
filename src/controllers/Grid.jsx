8873883edd173810f096e953fb98a55036bae515
22PHOP2D-Munkh-Uils <22phop2d-munkh-uils@Bilguuns-iMac.local>
2023-03-14 20:03:03 +0800

    ha

--- src/controllers/Grid.jsx
+++ src/controllers/Grid.jsx
@@ -12,29 +12,71 @@ import { DataContext } from "./context/Data.Provider";
 export const Grid = () => {
   const { menu } = useContext(DataContext);
   return (
-    <div className={ menu ? styles.container : `${styles.container_none}`}>
+    <div className={menu ? styles.container : `${styles.container_none}`}>
       <div className={styles.pin_container}>
         <div className={styles.card_extra_large}>
           <img className={styles.card_image} src={image1}></img>
+          <div className={styles.blur}>
+            <div className={styles.small_title}>7 Хоногийн Тойм</div>
+            <div className={styles.title}>
+              Гэнэтийн уналт + Тесла хот + Бинг тэсрэлт
+            </div>
+          </div>
         </div>
         <div className={styles.card_medium1}>
           <img className={styles.card_image} src={image4}></img>
+          <div className={styles.blur1}>
+            <div className={styles.small_title}>Чөлөөт</div>
+            <div className={styles.title}>
+              2023 оны Оскарын эзэн хэн байх вэ?: Кино
+            </div>
+          </div>
         </div>
         <div className={styles.card_medium2}>
           <img className={styles.card_image} src={image5}></img>
+          <div className={styles.blur2}>
+            <div className={styles.small_title}>Чөлөөт</div>
+            <div className={styles.title}>
+              NETFLIX-ийн хамгийн алдартай Хятад кинонууд
+            </div>
+          </div>
         </div>
         <div className={styles.card_large1}>
           <img className={styles.card_image} src={image6}></img>
+          <div className={styles.blur3}>
+            <div className={styles.small_title}>Мэдүүштэй</div>
+            <div className={styles.title}>
+              Байгальд ээлтэй, хүн төвтэй “ногоон зах”-ыг
+            </div>
+          </div>
         </div>
         <div className={styles.card_large2}>
           <img className={styles.card_image} src={image7}></img>
+          <div className={styles.blur3}>
+            <div className={styles.small_title}>Чөлөөт</div>
+            <div className={styles.title}>
+              Эмэгтэйчүүдийн эрхийг хамгаалах өдрийг улс
+            </div>
+          </div>
         </div>
         <div className={styles.card_small}>
           <div className={styles.small_small1}>
             <img className={styles.card_image} src={image2}></img>
+            <div className={styles.blur4}>
+              <div className={styles.small_title}>Номын тухай</div>
+              <div className={styles.title}>
+                Номтой яриа #32: Зөгийчин, орчуулагч
+              </div>
+            </div>
           </div>
           <div className={styles.small_small2}>
             <img className={styles.card_image} src={image3}></img>
+            <div className={styles.blur4}>
+              <div className={styles.small_title}>Аравт</div>
+              <div className={styles.title}>
+                Alpine цанын тамирчин, “X Team”-ийн гишүүн С.Тэ...
+              </div>
+            </div>
           </div>
         </div>
       </div>
