a97ed3d7edcc2f4cc089990604761645cda3d1bc
2022HOP2D-Turbold <turbold@LEAP-8s-iMac.local>
2023-03-14 19:37:23 +0800

    comment by turuu

--- src/component/News.js
+++ src/component/News.js
@@ -9,6 +9,7 @@ import axios from "axios";
 export const News = () => {
   const { id } = useParams();
   const [data, setData] = useState();
+  const [user, setUser] = useState();
   const ref = useRef(null);
   const [limit, setLimit] = useState([]);
   const [name, setName] = useState([]);
@@ -17,21 +18,25 @@ export const News = () => {
   const [ner, setNer] = useState(false);
   const [comment, setComment] = useState([]);
 
-  const Btn = () => {
+  const Btn = (id) => {
     var my_string = limit;
     var spaceCount = limit.split(" ").length - 1;
-    const charCount = my_string.length - spaceCount
+    const charCount = my_string.length - spaceCount;
     setSpace(charCount);
 
-    if (charCount < 100) {
+    if (charCount > 100) {
       console.log("less than 100");
       setWarn(false);
     } else {
-      console.log("100 gass ih");
+      const newComment = { limit: limit, name: name ? name : "Зочин" };
+      const commentDiv = { name: name, texts: limit, article: id };
+      setComment([...comment, newComment]);
       setWarn(true);
+      console.log(commentDiv);
+        axios.post("http://localhost:3100/comments/",commentDiv).then((res)=>{
+          console.log("Comment posted",res.data)
+        }).catch((err)=>{console.log(err)})
     }
-    const newComment = { limit: limit, name: name ? name : "Зочин" };
-    setComment([...comment, newComment]);
 
     if (!name) {
       console.log("noname");
@@ -41,20 +46,31 @@ export const News = () => {
       setNer(true);
     }
   };
-  
+
   useEffect(() => {
     axios
       .get(`http://localhost:3100/articles/${id}`)
       .then((res) => {
         console.log(res.data);
-        setData(res.data);
+        setData(res.data.result);
+        setComment(res.data.comment);
+        console.log("comment", res.data.comment);
+        axios
+          .get(`http://localhost:3100/user/${res.data.result.creatorId}`)
+          .then((res) => {
+            console.log(res.data);
+            setUser(res.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
   useEffect(() => {
-    if (data) ref.current.innerHTML = data.texts; 
+    if (data) ref.current.innerHTML = data.texts;
     console.log(ref);
   }, [data]);
   return (
@@ -85,15 +101,20 @@ export const News = () => {
           <div className={styles.width1}>
             <div style={{ display: "block" }}>
               <div className={styles.publish}>
-                <img
-                  src="https://m.media-amazon.com/images/M/MV5BMmQ3NzVmOWUtNTQ5Yi00MDczLWIxMDYtNGU5ZGU2YjI1NDY2XkEyXkFqcGdeQVRoaXJkUGFydHlJbmdlc3Rpb25Xb3JrZmxvdw@@._V1_.jpg"
-                  className={styles.pub_pic}
-                ></img>
+                {user && (
+                  <img
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+                    className={styles.pub_pic}
+                  ></img>
+                )}
+
                 <div className={styles.nii}>
                   <p style={{ color: "#888888", fontSize: "12px" }}>
                     Нийтэлсэн:
                   </p>
-                  <p style={{ fontSize: "15px" }}>B.Barjargal</p>
+                  <p style={{ fontSize: "15px" }}>
+                    {user && user.username.first + " " + user.username.last}
+                  </p>
                 </div>
               </div>
               <div className={styles.help}>
@@ -131,14 +152,16 @@ export const News = () => {
               >
                 Хандалт / Сэтгэгдэл:
               </p>
-              <div style={{  display: "flex",alignItems:"center",gap:"5px" }}>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+              >
                 <div style={{ marginRight: "20px" }}>
                   {" "}
                   <AiFillEye />
                   <span>{data.views}</span>
                 </div>
                 <FaComment />
-                <span>{data.comment.length}</span>
+                <span>{comment && comment.length}</span>
               </div>
               <div className={styles.main} ref={ref}></div>
             </div>
@@ -149,60 +172,62 @@ export const News = () => {
             </div>
           </div>
           <div className={styles.com}>
-          <div style={{ display: "flex", marginBottom: "20px" }}>
-            <img
-              src="https://unread.today/img/comment.png"
-              style={{
-                height: "26px",
-                marginTop: "10px",
-                marginRight: "7px",
-                fontFamily: "'SF Display', sans-serif",
+            <div style={{ display: "flex", marginBottom: "20px" }}>
+              <img
+                src="https://unread.today/img/comment.png"
+                style={{
+                  height: "26px",
+                  marginTop: "10px",
+                  marginRight: "7px",
+                  fontFamily: "'SF Display', sans-serif",
+                }}
+              ></img>
+              <h4>Сэтгэгдэл бичих</h4>
+            </div>
+            <div className={styles.con}>
+              <input
+                className={styles.input}
+                placeholder="таны нэр"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows="6"
+                cols="30"
+                className={styles.commentArea}
+                placeholder="сэтгэгдэл"
+                value={limit}
+                onChange={(e) => setLimit(e.target.value)}
+              ></textarea>
+            </div>
+            <button
+              className={styles.btn}
+              onClick={() => {
+                Btn(data._id);
               }}
-            ></img>
-            <h4>Сэтгэгдэл бичих</h4>
-          </div>
-          <div className={styles.con}>
-            <input
-              className={styles.input}
-              placeholder="таны нэр"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <textarea
-              id="w3review"
-              name="w3review"
-              rows="6"
-              cols="30"
-              placeholder="сэтгэгдэл"
-              value={limit}
-              onChange={(e) => setLimit(e.target.value)}
-            ></textarea>
-          </div>
-          <button
-            className={styles.btn}
-            onClick={Btn}
-            style={{
-              backgroundColor: warn ? "#0078f6" : "white",
-              color: warn ? "white" : "grey",
-            }}
-          >
-            Сэтгэгдэл бичих
-          </button>
-          <div className={styles.limit}>
-            <div className={styles.hhh}>
-              <span style={{ color: "#0078f6" }}>Тэмдэгт:{space}.</span>
-              <span>
-                Та 100-с доошгүй тэмдэгт ашиглан санал сэтгэгдлээ бүтээлчээр
-                үлдээгээрэй. Хамтдаа сэтгэгдэл бичих соёлыг Монголд бий
-                болгоцгооё.
-              </span>
+              style={{
+                backgroundColor: warn ? "#0078f6" : "white",
+                color: warn ? "white" : "grey",
+              }}
+            >
+              Сэтгэгдэл бичих
+            </button>
+            <div className={styles.limit}>
+              <div className={styles.hhh}>
+                <span style={{ color: "#0078f6" }}>Тэмдэгт:{space}.</span>
+                <span>
+                  Та 100-с доошгүй тэмдэгт ашиглан санал сэтгэгдлээ бүтээлчээр
+                  үлдээгээрэй. Хамтдаа сэтгэгдэл бичих соёлыг Монголд бий
+                  болгоцгооё.
+                </span>
+              </div>
             </div>
           </div>
-        </div>
         </div>
         <Footer />
       </>
     )
   );
 };
-
