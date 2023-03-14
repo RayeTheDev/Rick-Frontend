beda1e20a8fc24aa68c75ac7d60b96857a7017e0
2022HOP2D-Turbold <turbold@LEAP-8s-iMac.local>
2023-03-14 16:45:20 +0800

    as

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,35 +1,54 @@
-import styles from './styles/Carousel.module.css'
-import { Carousel } from '@mantine/carousel'
-import Autoplay from 'embla-carousel-autoplay'
-import { useRef } from 'react'
-import Card from 'react-bootstrap/Card'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import photo1 from './assets/image1.jpeg'
-// "style={{transform:`scale(${props.res}%)`}}"
+import styles from "./styles/Carousel.module.css";
+import { Carousel } from "@mantine/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { useEffect, useRef } from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import photo1 from "./assets/image1.jpeg";
+import axios from "axios";
+
 export const Carousell = (props) => {
-  const autoplay = useRef(Autoplay({ delay: 2500 }))
+  const autoplay = useRef(Autoplay({ delay: 2500 }));
 
   const ImgOverlayExample = (props) => {
+    useEffect(() => {
+      axios
+        .get("http://localhost:3100/articles")
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+
     return (
-      <Card className={`bg-dark text-white ${styles.CardImg}`} style={{backgroundImage:`url("${photo1}")`,backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition:"center",borderRadius:"15px"}} >
+      <Card
+        className={`bg-dark text-white ${styles.CardImg}`}
+        style={{
+          backgroundImage: `url("${photo1}")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          borderRadius: "15px",
+        }}
+      >
         <Card.ImgOverlay className={`${styles.Card}`}>
           <Card.Text>3 mins ago</Card.Text>
           <div>
             <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text.
-            </Card.Text>
+            <Card.Text>This is a wider card with supporting text.</Card.Text>
             <div>Techworm</div>
           </div>
         </Card.ImgOverlay>
       </Card>
-    )
-  }
+    );
+  };
   return (
     <div className={styles.containerCarousel}>
       <div className={styles.carouselText}>
-        <p className='text-white'>Редакцын сонголт</p>
-        <p/>
+        <p className="text-white">Редакцын сонголт</p>
+        <p />
       </div>
       <Carousel
         height={450}
@@ -38,15 +57,15 @@ export const Carousell = (props) => {
         loop
         align="start"
         breakpoints={[
-          { maxWidth: 'md', slideSize: '33.3333%' },
-          { maxWidth: 'xs', slideSize: '60%'},
+          { maxWidth: "md", slideSize: "33.3333%" },
+          { maxWidth: "xs", slideSize: "60%" },
         ]}
         withControls={false}
         plugins={[autoplay.current]}
         onMouseEnter={autoplay.current.stop}
         onMouseLeave={autoplay.current.reset}
         onClick={autoplay.current.reset}
-        style={{scrollSnapType:"x mandatory"}}
+        style={{ scrollSnapType: "x mandatory" }}
       >
         <Carousel.Slide className={styles.slide}>
           {ImgOverlayExample()}
@@ -77,5 +96,5 @@ export const Carousell = (props) => {
         </Carousel.Slide>
       </Carousel>
     </div>
-  )
-}
+  );
+};
